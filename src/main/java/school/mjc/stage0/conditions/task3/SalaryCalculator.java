4e8d70283f08529c1ea17df6c5aeba0dc0fe2167
sadyrin <sadyrin@gmail.com>
2023-01-25 23:44:02 +0200

    Done task stage0-module4-task3

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if(
+            salary > 0
+            && salary <= 10000
+        ){
+            System.out.println(salary * ((100 - 15.0) / 100.0));
+        }
+        else if(
+            salary > 10000
+            && salary <= 20000
+        ){
+            System.out.println(salary * ((100 - 18.0) / 100.0));
+        }
+        else if(
+            salary > 20000
+        ){
+            System.out.println(salary * ((100 - 20.0) / 100.0));
+        }
+            else System.out.println("wrong input!");
     }
 }
